087416688c6c5b9c2a5a28a766c0585fc8d8783e
bischoffhashemd <dimabischoffhashem@gmail.com>
2023-12-07 19:00:38 -0500

    adding photos to album

--- app/src/main/java/com/example/android29/AlbumDetailsActivity.java
+++ app/src/main/java/com/example/android29/AlbumDetailsActivity.java
@@ -1,19 +1,85 @@
 package com.example.android29;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Date;
 
 public class AlbumDetailsActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Album currentAlbum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_details);
 
+        // Assuming you have passed the current album to this activity
+        currentAlbum = (Album) getIntent().getSerializableExtra("album");
+
+        Button addPhotoButton = findViewById(R.id.addPhoto);
+        addPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+                //showAddPhotoDialog();
+            }
+        });
+
         showPhotos();
     }
 
-    private void showPhotos() {
 
+    private final ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Intent data = result.getData();
+                    Uri selectedImageUri = data.getData();
+
+                    // Now you can use the selectedImageUri to do whatever you need with the selected image
+                    // For example, you can create a Photo object and add it to the current album
+                    String imagePath = getPathFromUri(selectedImageUri);
+                    if (imagePath != null) {
+                        Photo selectedPhoto = new Photo(imagePath, "Caption", new Date());
+                        currentAlbum.addPhoto(selectedPhoto);
+                        // You may want to update your UI to display the newly added photo
+                    }
+                }
+            }
+    );
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryLauncher.launch(galleryIntent);
+    }
+
+    private String getPathFromUri(Uri contentUri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, projection, null, null, null);
+        if (cursor != null) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(columnIndex);
+            cursor.close();
+            return imagePath;
+        }
+        return null;
+    }
+
+    private void showPhotos() {
+        // Implement logic to display photos in your UI
     }
 }
+
